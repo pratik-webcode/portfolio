d24d04d8a42060f4559e0cb913da248d622242c2
pratik-webcode <pratikkolhatkar710@gmail.com>
2025-02-09 21:51:51 +0530

    add java Script file

---
+++ javascript.js
@@ -0,0 +1,33 @@
+
+gsap.from("#portfolio",{
+    y:-100,
+    opacity:0,
+    // delay:1,
+    duration:1,
+})
+
+gsap.from("#btn ",{
+    x:100,
+    opacity:0,
+    delay:1,
+    duration:1,
+})
+
+gsap.from("#btn2 ",{
+    x:-100,
+    opacity:0,
+    delay:1,
+    duration:1,
+})
+
+gsap.from("#firstHeading",{
+    x:100,
+    opacity:0,
+    delay:1,
+    duration:2,
+})
+
+gsap.from("#secondheading",{
+    opacity:0,
+    delay:3,
+})
